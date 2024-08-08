def4e09a22dc4e53de7f7f5b11ef71407c2d622b
Aryan Singh <as10071999@gmail.com>
2024-08-09 02:55:27 +0530

    added home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,41 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* Image Container */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image src={"/hero.png"} alt="" fill className="object-contain" />
+      </div>
+      {/* Text Container */}
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-2 md:gap-8  items-center justify-center relative">
+        <h1 className="text-4xl lg:text-6xl font-bold">
+          Turning Ideas into Impactful Solutions
+        </h1>
+        <p className="text-xl lg:text-2xl">
+          Full Stack Developer | Expert in React | Experience in MERN stack.
+        </p>
+        <p className="lg:text-xl">
+          Hello! I’m Aryan Singh, a dedicated Software Engineer with a strong
+          passion for solving real-world problems through technology. My primary
+          expertise lies in front-end development with React, but my curiosity
+          and drive have led me to explore backend technologies as well. My
+          ultimate goal is to become a proficient full-stack developer, bridging
+          the gap between front-end elegance and backend functionality to create
+          seamless, impactful applications.
+        </p>
+        {/* BUTTONS */}
+        <div className="flex gap-4 w-full">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black">
+            Contact Me
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
