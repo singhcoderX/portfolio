de45df8d912c5165b9ac02bca2e3fcbf90b8c76e
Aryan Singh <as10071999@gmail.com>
2024-09-03 21:34:22 +0530

    added biography text

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -9,7 +9,27 @@ const AboutPage = () => {
         {/* Text Container  */}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
           {/* Biography Container  */}
-          <div className="">Biography</div>
+          <div className="flex flex-col gap-12 justify-center">
+            <h1>Biography</h1>
+            <p>
+              Aryan Singh hails from the vibrant city of Kashipur in
+              Uttarakhand, India. With a strong academic foundation in science,
+              they embarked on their journey to IIT at a young age, beginning
+              preparations in Kota from Class 9th. Their hard work paid off when
+              they successfully cracked the IIT entrance exam, leading them to
+              pursue a B.Tech in Computer Science at IIT Jodhpur, from which
+              they graduated in 2021. Currently, they are a skilled full-stack
+              developer, boasting three years of experience, particularly with
+              React. Their passion for coding is matched by a love for football,
+              both as a player and a spectator. Beyond the digital realm, they
+              find joy in traveling and sketching, hobbies that allow them to
+              explore their creativity and the world around them. Driven by a
+              desire to live a healthy and fulfilling life, they are committed
+              to making a positive impact on their country and its people,
+              striving to give back in meaningful ways.
+            </p>
+            <span>Hardwork works where talent fails</span>
+          </div>
           {/* Skills Container  */}
           <div className="">Skills</div>
           {/* Experience Container  */}
