7f0014cae8f833ed87ad6069be55d39ab3f81884
Aryan Singh <as10071999@gmail.com>
2024-11-18 05:49:04 +0530

    Added Text container in Contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,40 @@
+"use client";
 import React from "react";
 import PageWrapper from "../../components/pageWrapper";
-import WorkInProgress from "../../components/workInProgress";
+import { motion } from "framer-motion";
 
 const ContactPage = () => {
+  const text = "Hi There";
+
   return (
     <PageWrapper>
-      {" "}
-      <WorkInProgress />
+      {/* <WorkInProgress /> */}
+      <div className="h-full flex flex-col lg:flex-row  px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER  */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
+          <div>
+            {text.split("").map((letter, idx) => {
+              return (
+                <motion.span
+                  key={idx}
+                  initial={{ opacity: 1 }}
+                  animate={{ opacity: 0 }}
+                  transition={{
+                    duration: 3,
+                    repeat: Infinity,
+                    delay: idx * 0.1,
+                  }}
+                >
+                  {letter}
+                </motion.span>
+              );
+            })}
+            <span>😎</span>
+          </div>
+        </div>
+        {/* FORM CONTAINER  */}
+        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+      </div>
     </PageWrapper>
   );
 };
