cbeb6d9816de37cef5657f3c8850fff4a935c02d
Aryan Singh <as10071999@gmail.com>
2024-11-19 04:14:55 +0530

    added Form in contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,17 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import PageWrapper from "../../components/pageWrapper";
 import { motion } from "framer-motion";
 
 const ContactPage = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
   const text = "Hi There";
 
+  const handleEmail = (e) => {
+    e.preventDefault();
+    setError(true);
+  };
   return (
     <PageWrapper>
       {/* <WorkInProgress /> */}
@@ -33,7 +39,35 @@ const ContactPage = () => {
           </div>
         </div>
         {/* FORM CONTAINER  */}
-        <div className="h-1/2 lg:h-full lg:w-1/2"></div>
+        <form
+          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
+          onSubmit={handleEmail}
+        >
+          <span>Dear Aryan Singh,</span>
+          <textarea
+            rows={6}
+            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+          ></textarea>
+          <span>My email address is:</span>
+          <input
+            type="email"
+            className="bg-transparent border-b-2 border-b-black outline-none"
+          />
+          <span>Thanks & Regards</span>
+          <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
+            Send
+          </button>
+          {success && (
+            <span className="text-green-600 font-semibold">
+              Your message has been sent successfully!
+            </span>
+          )}
+          {error && (
+            <span className="text-red-600 font-semibold">
+              Something went wrong!
+            </span>
+          )}
+        </form>
       </div>
     </PageWrapper>
   );
