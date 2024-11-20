f3637f6127d0309bccc1038bb57b8f2e720d8d20
Aryan Singh <as10071999@gmail.com>
2024-11-20 05:59:20 +0530

    added EmailJs

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,16 +1,40 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import PageWrapper from "../../components/pageWrapper";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const form = useRef();
   const text = "Hi There";
 
   const handleEmail = (e) => {
     e.preventDefault();
-    setError(true);
+    setError(false);
+    setSuccess(false);
+    console.log("PRocess", process.env.NEXT_PUBLIC_PUBLIC_KEY);
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+          setSuccess(true);
+          form.current.reset();
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          setError(true);
+        }
+      );
   };
   return (
     <PageWrapper>
@@ -42,16 +66,19 @@ const ContactPage = () => {
         <form
           className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24"
           onSubmit={handleEmail}
+          ref={form}
         >
           <span>Dear Aryan Singh,</span>
           <textarea
             rows={6}
             className="bg-transparent border-b-2 border-b-black outline-none resize-none"
+            name="user_message"
           ></textarea>
           <span>My email address is:</span>
           <input
             type="email"
             className="bg-transparent border-b-2 border-b-black outline-none"
+            name="user_email"
           />
           <span>Thanks & Regards</span>
           <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4">
